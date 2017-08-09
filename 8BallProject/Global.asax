32cf8a2ca907082b085e270323e8e597fa22510a
Woodsmike <Woodsmike@users.noreply.github.com>
2017-08-09 10:54:54 -0400

    Initial commit

---
+++ 8BallProject/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="_8BallProject.MvcApplication" Language="C#" %>
---
+++ 8BallProject/Scripts/Scripts.js
@@ -0,0 +1,12 @@
+﻿var phrases = ["Feeling Lucky", "Yes", "No", "Ask Again", "I Don't Know", "Maybe", "Definitely", "Most Likely", "Try Again", "Doubtful", "Heck No!"];
+
+function getPhrase() {
+    var phrase = phrases[Math.floor(Math.random() * 11)];
+    return phrase;
+}
+function givePhrase() {
+    document.getElementById("displayPhrase").innerHTML = getPhrase();
+}
+
+
+
